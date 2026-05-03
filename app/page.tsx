b49058f968b728654870ba5b31af9633d7b880bb
svsxn <sebmagn102@gmail.com>
2026-05-03 19:11:24 +0200

    feat: add hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,29 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="flex justify-center w-full pt-24 mx-8">
+      <section id="hero">
+        <div className="flex flex-col items-center gap-6">
+          <div>
+            <Image
+              width={256}
+              height={256}
+              alt="Profile picture"
+              src="/profile-picture.png"
+              className="rounded-full"
+            />
+          </div>
+          <div className="flex flex-col items-center">
+            <span className="text-center">
+              Full-Stack Web Developer | Next.js & Supabase
+            </span>
+            <h1 className="text-6xl font-bold text-center">
+              Sebastian Magnucki
+            </h1>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
