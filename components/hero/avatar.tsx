791c784cb6cb00aa62e183a13bdf8935f5eaaf8a
svsxn <sebmagn102@gmail.com>
2026-05-08 14:47:49 +0200

    feat: add responsiveness

--- components/hero/avatar.tsx
+++ components/hero/avatar.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 export function Avatar() {
   return (
-    <div className="flex justify-end min-w-105">
+    <div className="order-1 md:order-2 flex md:justify-end">
       <Image
         width={420}
         height={420}
         alt="Avatar"
         src="/avatar.png"
         loading="eager"
-        className="rounded-full size-105 border border-white/10 shadow-[0_0_80px_rgba(255,255,255,0.05)] animate-in fade-in zoom-in-95 duration-700 delay-150"
+        className="rounded-full md:size-105 border border-white/10 shadow-[0_0_80px_rgba(255,255,255,0.05)] animate-in fade-in zoom-in-95 duration-700 delay-150"
       />
     </div>
   );
