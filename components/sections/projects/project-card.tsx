2821b502aa9e5753aac930b12ad4196165e98ecf
svsxn <sebmagn102@gmail.com>
2026-05-11 14:31:40 +0200

    fix: responsiveness

--- components/sections/projects/project-card.tsx
+++ components/sections/projects/project-card.tsx
@@ -4,8 +4,8 @@ import Label from "../label";
 
 export default function ProjectCard() {
   return (
-    <div className="flex flex-col md:flex-row justify-between border rounded-xl bg-linear-to-t from-card from-70% to-white/10">
-      <div className="order-2 md:order-1 flex flex-col gap-4 py-6 pl-8">
+    <div className="flex flex-col md:flex-row justify-between md:gap-6 border rounded-xl bg-linear-to-t from-card from-70% to-white/10">
+      <div className="order-2 md:order-1 flex flex-col gap-4 py-5 md:py-6 px-4 md:px-0 md:pl-8">
         <div className="flex flex-col gap-2">
           <Label primaryText="SAAS Dashboard" />
           <h3 className="text-2xl font-semibold tracking-tight">
@@ -24,7 +24,7 @@ export default function ProjectCard() {
           </div>
           <div className="text-xs bg-muted px-2 py-1 rounded-sm">Tailwind</div>
         </div>
-        <div className="flex gap-6">
+        <div className="flex flex-col sm:flex-row gap-2 sm:gap-6">
           <Button className="text-[13px] px-6 py-5 rounded-sm hover:brightness-110 hover:-translate-y-0.5">
             Live Demo
             <ArrowUpRightIcon className="ml-3" />
@@ -38,7 +38,7 @@ export default function ProjectCard() {
           </Button>
         </div>
       </div>
-      <div className="order-1 md:order-2 w-180 h-65 bg-neutral-800 place-self-end rounded-md mask-[linear-gradient(to_bottom,black_90%,transparent_100%)] my-2 mr-2"></div>
+      <div className="order-1 md:order-2 w-180 max-w-full h-65 bg-neutral-800 place-self-end rounded-md mask-[linear-gradient(to_bottom,black_90%,transparent_100%)] md:my-2 md:mr-2"></div>
     </div>
   );
 }
