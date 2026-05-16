ab64f3bff19a891f3d5f733028ff120f5a7c49cb
svsxn <sebmagn102@gmail.com>
2026-05-16 20:44:10 +0200

    feat: update portrait

--- components/sections/hero/index.tsx
+++ components/sections/hero/index.tsx
@@ -1,4 +1,4 @@
-import { Avatar } from "./avatar";
+import Portrait from "./portrait";
 import Background from "./background";
 import Content from "./content";
 
@@ -11,7 +11,7 @@ export default function Hero() {
       <Background />
       <div className="mx-auto grid min-h-[calc(100svh-5rem)] w-full max-w-6xl items-center px-6 py-16 md:py-20 lg:grid-cols-[1fr_0.92fr] lg:gap-14 lg:py-24">
         <Content />
-        <Avatar />
+        <Portrait />
       </div>
     </section>
   );
