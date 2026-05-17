2908abe80a595afcbf3aafd049fd22722eb13aa3
svsxn <sebmagn102@gmail.com>
2026-05-17 11:34:16 +0200

    feat: hide projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/sections/hero";
 import Projects from "@/components/sections/projects";
+// import Projects from "@/components/sections/projects-old";
 import Skills from "@/components/sections/skills";
 
 export default function Home() {
