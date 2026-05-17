e21f2f529cdf7a5279977c61bacfe4a90ac920a4
svsxn <sebmagn102@gmail.com>
2026-05-17 16:53:30 +0200

    feat: hide skills

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import Hero from "@/components/sections/hero";
 import Projects from "@/components/sections/projects";
 // import Projects from "@/components/sections/projects-old";
-import Skills from "@/components/sections/skills";
+// import Skills from "@/components/sections/skills";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Projects />
-      <Skills />
+      {/* <Skills /> */}
     </>
   );
 }
