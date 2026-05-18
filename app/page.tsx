e6692c5f405e215cb07423170b6c3e9a93a3cbc9
svsxn <sebmagn102@gmail.com>
2026-05-18 15:06:33 +0200

    refactor: delete old projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Hero from "@/components/sections/hero";
 import Projects from "@/components/sections/projects";
-// import Projects from "@/components/sections/projects-old";
 // import Skills from "@/components/sections/skills";
 
 export default function Home() {
